03b79482d1848ef3fc7d0a8242edb325e68910d7
Ahmed Alaa <ahmedjoey99@gmail.com>
2022-02-17 19:15:09 +0200

    Adding Latest Project

--- crowdfund/crowdfund/urls.py
+++ crowdfund/crowdfund/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
     path('users/', include('users.urls')),
     path('project_api/',include('apis.urls',namespace='crowd_funding_apis')),
     path('user_api/',include('user_apis.urls',namespace='user_apis')),
