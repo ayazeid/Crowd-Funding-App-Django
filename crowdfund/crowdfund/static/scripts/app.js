92da68dc11dad5b2ed09c4c9d184d056194a5a4c
Ahmed Alaa <ahmedjoey99@gmail.com>
2022-02-18 14:47:00 +0200

    Top Rated Projects Image Slider

--- crowdfund/crowdfund/static/scripts/app.js
+++ crowdfund/crowdfund/static/scripts/app.js
@@ -19,3 +19,42 @@ picPrompts.on("change", () => {
 
 // Show only the first prompt on load
 dynamicPicPromptsDisplay();
+
+//  Home Page Image Slider
+let slideIndex = 1;
+let myIndex = 0;
+
+function showDivs(n) {
+  let i;
+  const x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
+
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+function carousel() {
+  let i;
+  const x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 5000); // Change image every 5 seconds
+}
+carousel();
