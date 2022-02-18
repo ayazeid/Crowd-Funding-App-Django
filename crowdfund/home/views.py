e89ce50d64eefd5413cef6c402ba1ae526d653b8
Ahmed Alaa <ahmedjoey99@gmail.com>
2022-02-18 19:41:43 +0200

    handling Search Error

--- crowdfund/home/views.py
+++ crowdfund/home/views.py
@@ -29,9 +29,6 @@ def projectCategories(request):
 
 def SearchTitle(request):
     title=request.POST.get('title')
-    if title:
-        search_by_title =Project.objects.filter(title__contains=title)
-        return render(request,'home/searchTitle.html' ,{"search_by_title":search_by_title})
-    else:
-        search_by_title ="No projects found with that title"
-        return render(request,'home/searchTitle.html' ,{search_by_title})
+    search_by_title =Project.objects.filter(title__contains=title)
+    return render(request,'home/searchTitle.html' ,{"search_by_title":search_by_title})
+    
