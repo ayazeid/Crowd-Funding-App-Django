fde6ae116a188be4c13e67e1679e9d25fa3de43c
ayazeid <aya.zeid95@gmail.com>
2022-02-19 04:20:34 +0200

    Handle migrations errors

--- crowdfund/crowdfund/settings.py
+++ crowdfund/crowdfund/settings.py
@@ -11,13 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import environ
+#import environ
 import os
 
 
 # Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
+# env = environ.Env()
+# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,27 +89,27 @@ WSGI_APPLICATION = 'crowdfund.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': env('DATABASE_NAME'),
-         'USER': env('DATABASE_USER'),
-         'PASSWORD': env('DATABASE_PASS'),
-         'HOST': 'localhost',
-         'PORT': 5432
-     }
- }
-
 # DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'crowddb',
-#        'USER': 'yoda',
-#        'PASSWORD': 'yoda',
-#        'HOST': 'localhost',
-#        'PORT': 5432
-#    }
-# }
+#      'default': {
+#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#          'NAME': env('DATABASE_NAME'),
+#          'USER': env('DATABASE_USER'),
+#          'PASSWORD': env('DATABASE_PASS'),
+#          'HOST': 'localhost',
+#          'PORT': 5432
+#      }
+#  }
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'crowdfund',
+       'USER': 'yoda',
+       'PASSWORD': 'yoda',
+       'HOST': 'localhost',
+       'PORT': 5432
+   }
+}
 
 
 
@@ -174,7 +174,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_USE_TLS = True  
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_HOST_USER = 'crowd.funding.iti.website@gmail.com'  
-EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')  
+EMAIL_HOST_PASSWORD = 'Crowdfunding1'
 EMAIL_PORT = 587  
 
 
